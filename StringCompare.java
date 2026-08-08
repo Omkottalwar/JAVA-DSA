2b9396e42fe245fe7385e9454cbbbdff69304db4
omkottalwar <kottalwarom@gmail.com>
2026-08-09 00:01:34 +0530

    String Builders

--- StringCompare.java
+++ StringCompare.java
@@ -3,7 +3,7 @@ public class StringCompare{
         String str[]={"mango","apple","banana"};
         String largest=str[0];
         for(int i=0; i<str.length; i++){
-            if(largest.compareTo(str[i])< 0){
+            if(largest.compareTo(str[i])<0){
                 largest=str[i];
             }
         }
