3934d110732bc599b877dca74731701cd4b4509c
omkottalwar <kottalwarom@gmail.com>
2026-08-08 23:36:36 +0530

    lexicographic comaprison of strings

---
+++ StringCompare.java
@@ -0,0 +1,12 @@
+public class StringCompare{
+    public static void main(String args[]){
+        String str[]={"mango","apple","banana"};
+        String largest=str[0];
+        for(int i=0; i<str.length; i++){
+            if(largest.compareTo(str[i])< 0){
+                largest=str[i];
+            }
+        }
+        System.out.print(largest);
+    }
+}
